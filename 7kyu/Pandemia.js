ebf7c56e2e08ab2f94385e19834ece29e1246785
Paola Garay <garay.angela@gmail.com>
2020-08-20 10:09:49 +0200

    Sort Numbers (7kyu)

--- 7kyu/Pandemia.js
+++ 7kyu/Pandemia.js
@@ -1,7 +1,6 @@
 /*
 https://www.codewars.com/kata/5e2596a9ad937f002e510435/train/javascript
 Instructions:
-
 ⚠️ The world is in quarantine! There is a new pandemia that struggles mankind. Each continent is isolated from each other but infected people have spread before the warning. ⚠️
 🗺️ You would be given a map of the world in a type of string:
 string s = "01000000X000X011X0X"
---
+++ 7kyu/SortNumbers.js
@@ -0,0 +1,18 @@
+/*
+https://www.codewars.com/kata/5174a4c0f2769dd8b1000003/train/javascript
+Instructions:
+
+Finish the solution so that it sorts the passed in array of numbers. If the function passes in an empty array or null/nil value then it should return an empty array.
+For example:
+solution([1, 2, 10, 50, 5]); // should return [1,2,5,10,50]
+solution(null); // should return []
+*/
+
+const solution = (nums) => { 
+    if (nums === undefined || nums === null) return [];
+        else return nums.sort((a, b) => a - b);
+ }
+ 
+ //Test:
+ solution([1, 2, 10, 50, 5])
+ //solution(null); 
