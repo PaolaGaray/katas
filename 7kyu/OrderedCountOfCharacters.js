0ee5704ce92621c6efb7e54c88d2de745566729d
Paola Garay <garay.angela@gmail.com>
2020-08-22 09:31:46 +0200

    Ordered Count of Characters (7kyu)

--- 7kyu/OrderedCountOfCharacters.js
+++ 7kyu/OrderedCountOfCharacters.js
@@ -6,19 +6,26 @@ Example:
 orderedCount("abracadabra") == [['a', 5], ['b', 2], ['r', 2], ['c', 1], ['d', 1]]
 */
 
-var orderedCount = function (text) {
-    // Implement me!
-    return [];
-  }
-
+const orderedCount = (text) => {
+  const arr = text.split('');
+  const noDuplicates = arr.filter((ele, index) => {
+      return arr.indexOf(ele) === index;
+  });
+  let result = [];
+  noDuplicates.forEach((el) => {
+      const occurences = text.split(el).length - 1;
+      result.push([el, occurences])
+  })
+  return result;
+}
 
 
 //Test
 orderedCount("abracadabra")
 //[ ['a', 5], ['b', 2], ['r', 2], ['c', 1], ['d', 1] ]
 
-orderedCount("Code Wars")
+//orderedCount("Code Wars")
 // [ ['C', 1], ['o', 1], ['d', 1], ['e', 1], [' ', 1], ['W', 1], ['a', 1], ['r', 1], ['s', 1] ]
 
-orderedCount("233312")
+//orderedCount("233312")
 //[ ['2', 2], ['3', 3], ['1', 1 ] ]
