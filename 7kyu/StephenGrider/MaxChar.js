e0ea28fe0754dfe71f0bb3c8c65f07f7710b79ce
Paola Garay <garay.angela@gmail.com>
2020-11-11 16:37:20 -0500

    New solution for MaxChar problem (7kyu)

--- 7kyu/StephenGrider/MaxChar.js
+++ 7kyu/StephenGrider/MaxChar.js
@@ -7,28 +7,57 @@
 
 
 
+// const maxChar = (str) => {
+//     const chars = {};
+//     for(let char of str) {
+//       if(!chars[char]) {
+//         chars[char] = 1
+//       } else {
+//         chars[char]++
+//       }
+//     }
+  
+//     const sortable = [];
+//     for(let char in chars){
+//       sortable.push([char, chars[char]]);
+//     }
+//     sortable.sort(function(a, b) {
+//       return b[1] - a[1];
+//     });
+    
+//     return sortable[0][0]
+    
+//   };
+  
+
+//   maxChar('abcccccccd')
+
+
+
+
+  //Other solution
 const maxChar = (str) => {
-    const chars = {};
-    for(let char of str) {
-      if(!chars[char]) {
-        chars[char] = 1
-      } else {
-        chars[char]++
-      }
+    //create an object with number of repetitions of each char:
+    const charMap = {};
+    for (let char of str) {
+        if(!charMap[char]) {
+            charMap[char] = 1;
+        } else {
+            charMap[char]++
+        }
     }
-  
-    
-    const sortable = [];
-    for(let char in chars){
-      sortable.push([char, chars[char]]);
+    //iterate through the object created and look for the max 
+    let max = 0;
+    let maxChar = '';
+
+    for(let char in charMap) {
+        if(charMap[char] > max) {
+            max = charMap[char];
+            maxChar = char;
+        }
     }
-    sortable.sort(function(a, b) {
-      return b[1] - a[1];
-    });
-    
-    return sortable[0][0]
-    
-  };
-  
-  
-  maxChar('abcccccccd')
+
+    return maxChar;
+}
+
+
