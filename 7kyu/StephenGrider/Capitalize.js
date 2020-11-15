0a87e0617fce8ae808ee5c33757872ec3a1a64e9
Paola Garay <garay.angela@gmail.com>
2020-11-15 10:15:31 -0500

    Capitalize (7kyu)

---
+++ 7kyu/StephenGrider/Capitalize.js
@@ -0,0 +1,31 @@
+// --- Directions
+// Write a function that accepts a string.  The function should
+// capitalize the first letter of each word in the string then
+// return the capitalized string.
+// --- Examples
+//   capitalize('a short sentence') --> 'A Short Sentence'
+//   capitalize('a lazy fox') --> 'A Lazy Fox'
+//   capitalize('look, it is working!') --> 'Look, It Is Working!'
+
+const capitalize = (str) => {
+    let result = [];
+    for(let word of str.split(' ')){
+        result.push(word[0].toUpperCase() + word.slice(1));
+    }
+    return result.join(' ');
+  }
+
+//capitalize('a short sentence')
+//'A Short Sentence'
+
+//   capitalize('a lazy fox')
+//'A Lazy Fox'
+
+capitalize('look, it is working!') 
+//'Look, It Is Working!'
+
+
+
+
+//Another solutions:
+
